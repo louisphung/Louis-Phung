7283f53ee69e7475ad22397fd2ce35eabf8658d4
louisphung <2342102@learn.vsb.bc.ca>
2021-02-12 19:05:51 -0800

    Unit 1 HW

---
+++ Unit 1/src/MAIN.java
@@ -0,0 +1,51 @@
+public class MAIN {
+    public static void main( String[] args )
+    {
+        //Problem 1
+        int i;
+        double y;
+
+            i = 113;
+            y = 2.71828;
+            String name = "Computer Science";
+            System.out.println(" Problem 1");
+        System.out.println( "This is room #" + i);
+        System.out.println("It close to " + y);
+        System.out.println("I am learning a bit about "+ name);
+        System.out.println();
+
+        //Problem 2
+        String a="Spanish 11";
+        String b="Programming 11";
+        String c="English 12";
+        String d="Entrepreneurship 12";
+        String e="Social Studies 11";
+        String f="Physic 12";
+        String g="Pre-Calculus 12";
+        String h="ELD 10";
+
+        String a1="Ms. Fogale, Cristina";
+        String b1="Mr. Zaremba, Paul";
+        String c1="Mrs. Maitland, Jenn";
+        String d1="Ms. Young, Amy";
+        String e1="Mrs. Quon, Jean";
+        String f1="Mr. Hamlen, Glen";
+        String g1="Mrs. Przyborowska, Margaret";
+        String h1="Mr. Zess";
+        System.out.println("+--------------------------------------------------+");
+        System.out.println("|1|         "+a+"|        "+a1+"|");
+        System.out.println("|2|     "+b+"|           "+b1+"|");
+        System.out.println("|3|         "+c+"|         "+c1+"|");
+        System.out.println("|4|"+d+"|              "+d1+"|");
+        System.out.println("|5|  "+e+"|             "+e1+"|");
+        System.out.println("|6|          "+f+"|            "+f1+"|");
+        System.out.println("|7|    "+g+"| "+g1+"|");
+        System.out.println("|8|             "+h+"|                    "+h1+"|");
+        System.out.println("+--------------------------------------------------+");
+
+
+
+
+    }
+
+}
